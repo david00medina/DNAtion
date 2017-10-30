c0eac74f7a910fc91ce3f3f80b0b89a7387cf108
David A. Medina Medina <david00medina@gmail.com>
2017-10-30 14:32:56 +0000

    DNAtion v1.0
    This version just implement DNA alignments with Burrows-Wheeler Toolkit. More up to come.

--- src/org/DNAtion/control/alignment/Picard/Picard.java
+++ src/org/DNAtion/control/alignment/Picard/Picard.java
@@ -1,4 +1,42 @@
 package org.DNAtion.control.alignment.Picard;
 
+import htsjdk.samtools.SAMFileHeader;
+import picard.sam.SortSam;
+
+import java.io.File;
+
 public class Picard {
+    private File sam;
+    private File bam;
+    private File sortedBam;
+
+    public Picard(File sam, File bam) {
+        this.sam = sam;
+        this.bam = bam;
+        this.sortedBam = new File(sam.getAbsolutePath().toLowerCase().
+                substring(0, sam.getAbsolutePath().indexOf(".sam")) + "_sorted.sam");
+    }
+
+    public void sortAndConvert() {
+        SortSam sortSam = new SortSam();
+        sortSam.INPUT = sam;
+        sortSam.OUTPUT = sortedBam;
+        sortSam.SORT_ORDER = SAMFileHeader.SortOrder.coordinate;
+    }
+
+    public File getSam() {
+        return sam;
+    }
+
+    public void setSam(File sam) {
+        this.sam = sam;
+    }
+
+    public File getBam() {
+        return bam;
+    }
+
+    public void setBam(File bam) {
+        this.bam = bam;
+    }
 }
